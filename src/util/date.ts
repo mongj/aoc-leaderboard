5d68a0c643ec40732f571af3a7c149581569700f
Ming Jun <evan.zhangmingjun@gmail.com>
2024-12-04 00:20:32 +0800

    fix: advent timezone

--- src/util/date.ts
+++ src/util/date.ts
@@ -1,6 +1,8 @@
 // Get the current day in the Advent Calendar
 function getCurrentAdventDay() {
   const now = new Date();
+  // Adjust offset since AOC starts at midnight EST/UTC-5
+  now.setHours(now.getHours() - 13);
   const day = now.getDate();
   return day;
 }
